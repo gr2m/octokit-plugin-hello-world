34737c7a84d29e3f2a298099c00c6c38d447ac6b
Gregor Martynus <gregor@martynus.net>
2019-10-12 00:14:26 -0700

    ci(test): initial version

--- test/smoke.test.ts
+++ test/smoke.test.ts
@@ -1,7 +1,15 @@
-import { plugin } from "../src";
+import { helloWorld } from "../src";
 
 describe("Smoke test", () => {
-  it("is a function", () => {
-    expect(plugin).toBeInstanceOf(Function);
+  it("{ helloWorld } export is a function", () => {
+    expect(helloWorld).toBeInstanceOf(Function);
+  });
+
+  it("helloWorld()", () => {
+    expect(helloWorld).not.toThrow();
+  });
+
+  it("helloWorld.VERSION exists", () => {
+    expect(helloWorld.VERSION).toEqual("0.0.0-development");
   });
 });
