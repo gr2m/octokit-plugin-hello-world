74eac25e7e528cc274efff5cba724d071adf66c1
Gregor Martynus <gregor@martynus.net>
2019-10-12 23:01:08 -0700

    test: `octokit.helloWorld()` logs "Hello, world" with configurable greeting

--- test/smoke.test.ts
+++ test/smoke.test.ts
@@ -1,15 +1,21 @@
+import { Octokit } from "@octokit/core";
+
 import { helloWorld } from "../src";
+
+const TestOctokit = Octokit.plugin(helloWorld);
 
 describe("Smoke test", () => {
   it("{ helloWorld } export is a function", () => {
     expect(helloWorld).toBeInstanceOf(Function);
   });
 
-  it("helloWorld()", () => {
-    expect(helloWorld).not.toThrow();
-  });
-
   it("helloWorld.VERSION exists", () => {
     expect(helloWorld.VERSION).toEqual("0.0.0-development");
+  });
+
+  it("octokit.helloWorld()", () => {
+    jest.spyOn(console, "log").mockImplementation();
+    const octokit = new TestOctokit();
+    expect(octokit.helloWorld).not.toThrow();
   });
 });
