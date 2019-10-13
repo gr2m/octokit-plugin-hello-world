1fe4910f24c461fa1a4baad3f19f8329595aa7f6
Gregor Martynus <gregor@martynus.net>
2019-10-12 23:02:32 -0700

    feat: `octokit.helloWorld()` logs "Hello, world" with configurable greeting via `new Octokit({ greeting })

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,31 @@
 import { VERSION } from "./version";
 
-export function helloWorld() {}
+type Octokit = any;
+type HelloWorldPluginOptions = {
+  greeting?: string;
+  [key: string]: any;
+};
+
+/**
+ * `helloWorld` is a demo Octokit plugin to show it works :)
+ * It adds a new `octokit.helloWorld()` method which logs "Hello, World" by default.
+ * The greeting can be configured by passing a `greeting` option to the Octokit constructor.
+ *
+ * Example
+ *
+ * ```js
+ * const octokit = new Octokit({ greeting: "Hi" })
+ * octokit.helloWorld()
+ * // logs "Hi, world!"
+ * ```
+ *
+ * @param octokit Octokit instance
+ * @param options Options passed to Octokit constructor
+ */
+export function helloWorld(
+  octokit: Octokit,
+  { greeting = "Hello" }: HelloWorldPluginOptions
+) {
+  octokit.helloWorld = () => console.log(`${greeting}, world!`);
+}
 helloWorld.VERSION = VERSION;
